7465a131bbf43c00849eaa3ceb1b3f5350966414
alimov1201 <izzatjon443@gmail.com>
2024-07-18 08:24:53 +0500

    chat gpt bot qilish juda oson. Siz shunchaki openai kutubxonasi bilan ishlashni bilsangiz shu yetarli

---
+++ main.py
@@ -0,0 +1,72 @@
+from aiogram import Bot, Dispatcher, F, filters, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
+import asyncio
+import openai
+
+openai.api_key = "sk-proj-5gkPL1XPwqRyu7vGuJwzT3BlbkFJmC6ZH1W73HxJBgYSWY77"
+bot = Bot(token="7353339504:AAGsRb1hPc0RqcDlSkrGoQ0-gZpScCCt5SI")
+dp = Dispatcher(bot=bot)
+
+kb = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="President of USA"), KeyboardButton(text="President of Russia"), KeyboardButton(text="President of Uzbekistan")],
+    [KeyboardButton(text="Elon Musk"), KeyboardButton(text="Steve Jobs"), KeyboardButton(text="MrBeast")],
+    [KeyboardButton(text="Eminem"), KeyboardButton(text="Mike Tyson"), KeyboardButton(text="Cristiano Ronaldo")]
+],resize_keyboard=True)
+
+@dp.message(filters.CommandStart())
+async def start_bot(message: types.Message):
+    await message.answer(text="Hello, I'm ChatGPT bot\nselect the person you want to chat with", reply_markup=kb)
+
+@dp.message(F.text == "President of USA")
+async def usa_president(message: Message):
+    await message.answer(text="I'm looking for oil now, bye")
+
+@dp.message(F.text == "President of Russia")
+async def usa_president(message: Message):
+    await message.answer(text="президент россии не может с тобой общаться")
+
+@dp.message(F.text == "President of Uzbekistan")
+async def usa_president(message: Message):
+    await message.answer(text="Paxta bizning milliy boyligimiz")
+
+@dp.message(F.text == "Elon Musk")
+async def usa_president(message: Message):
+    await message.answer(text="This is an answering machine, leave a message")
+
+@dp.message(F.text == "Steve Jobs")
+async def usa_president(message: Message):
+    await message.answer(text="you need to buy an iPhone")
+
+@dp.message(F.text == "MrBeast")
+async def usa_president(message: Message):
+    await message.answer(text="next video on YouTube next week")
+
+@dp.message(F.text == "Eminem")
+async def usa_president(message: Message):
+    await message.answer(text="do you want to rap? go to karaoke")
+
+@dp.message(F.text == "Mike Tyson")
+async def usa_president(message: Message):
+    await message.answer(text="I don't want to be your trainer bye")
+
+@dp.message(F.text == "Cristiano Ronaldo")
+async def usa_president(message: Message):
+    await message.answer(text="not today next time, okay?")
+
+@dp.message()
+async def gpt_bot(message: types.Message):
+    user_input = message.text
+    response = openai.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=user_input,
+        max_tokens=1000,
+        temperature=0.7,
+    )
+    await message.answer(text=f"{response.choices[0].text.strip()}")
+     
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
